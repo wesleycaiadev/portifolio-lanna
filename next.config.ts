cba9ec94a402a41739dc8837e3b804df7dddf348
wesleycaiadev <wesleycaia.dev@gmail.com>
2026-05-22 22:37:20 -0300

    fix(config): corrige Next 15 image block e allowDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "cdn.pixabay.com" },
       { protocol: "https", hostname: "cdn.myportfolio.com" },
     ],
   },
+  allowedDevOrigins: ["192.168.0.9", "localhost", "http://192.168.0.9:3000"],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.glsl$/,
